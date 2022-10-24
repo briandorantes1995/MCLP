0d15445a30e400ad4ff10393941f1a7609e97459
Brian <briandorantes@hotmail.com>
2022-10-23 20:37:24 -0500

    Distancia euclidiana

--- MCLP.py
+++ MCLP.py
@@ -3,10 +3,13 @@ import sys
 import time
 from sklearn.metrics.pairwise import euclidean_distances
 
-
-
-instance = "inc50-1.csv"
-instance2 = "inp50-1.csv"
+if not sys.argv:
+    instance = "inc50-1.csv"
+    instance2 = "inp50-1.csv"
+else:
+    print("\nYou choose this instance:" + sys.argv[1])
+    instance = str(sys.argv[1])
+    instance2 = str(sys.argv[2])
 
 
 time_start = time.perf_counter()
@@ -23,7 +26,8 @@ costumers.rename(columns={0: 'X', 1: 'Y', 2: 'Demand'}, inplace=True)
 costumers.index += 1
 costumerscoords = costumers[['X','Y']]
 
-
+maximumdistance = input("Enter the maximum distance in meters for the node to be covered(meters)")
+facilities = input("Enter the facilities to be placed in the available locations")
 # Compute distances 
 
 dist2 = euclidean_distances(posiblelocations, costumerscoords)
@@ -31,6 +35,7 @@ dist2 = euclidean_distances(posiblelocations, costumerscoords)
 # Convert back to dataframe.
 
 df2 = pd.DataFrame( dist2, columns=costumers.index, index=posiblelocations.index)
+
+given_set = {maximumdistance}
+df2['coverednodes'] = df2.isin(given_set).sum(1)
 print(df2.head())
-
-
