48eeb737991dffb02813e8b4f37b88ff66bf76a7
Brian <briandorantes@hotmail.com>
2022-10-23 19:40:14 -0500

    Distancia euclidiana

--- MCLP.py
+++ MCLP.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import sys
 import time
-
+from sklearn.metrics.pairwise import euclidean_distances
 
 
 
@@ -10,16 +10,27 @@ instance2 = "inp50-1.csv"
 
 
 time_start = time.perf_counter()
-posiblelcoations = pd.read_csv(instance2)
-info = list(posiblelcoations.columns)
+posiblelocations = pd.read_csv(instance2)
+info = list(posiblelocations.columns)
 n = int(info[0])
 m = int(info[1])
-posiblelcoations.rename(columns={info[0]: 'X', info[1]: 'Y'}, inplace=True)
-posiblelcoations.index += 1
-print(posiblelcoations)
+posiblelocations.rename(columns={info[0]: 'X', info[1]: 'Y'}, inplace=True)
+posiblelocations.index += 1
+
 
 costumers = pd.read_csv(instance,header=None)
 costumers.rename(columns={0: 'X', 1: 'Y', 2: 'Demand'}, inplace=True)
 costumers.index += 1
-print(costumers)
+costumerscoords = costumers[['X','Y']]
+
+
+# Compute distances 
+
+dist2 = euclidean_distances(posiblelocations, costumerscoords)
+
+# Convert back to dataframe.
+
+df2 = pd.DataFrame( dist2, columns=costumers.index, index=posiblelocations.index)
+print(df2.head())
+
 
